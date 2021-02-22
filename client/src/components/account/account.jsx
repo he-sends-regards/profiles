4e74133f2fa19c176af43cca15810db7a9eff839
postscriptumno <postscriptum.no@gmail.com>
2021-02-22 18:46:39 +0200

    feat(react-router-dom): add link to home page

--- client/src/components/account/account.jsx
+++ client/src/components/account/account.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Account = () => {
   return (
-    <div className="account"></div>
+    <div className="account">
+      <Link to='/' style={{textDecoration: 'none', fontSize: '60px'}}>
+        <span>&#8592;</span>
+      </Link>
+    </div>
   );
 };
 
